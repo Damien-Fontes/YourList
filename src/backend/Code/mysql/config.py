5dc7741b9d522ffc393145af61a06454ab243a8f
FlorianCoquillat <114492728+FlorianCoquillat@users.noreply.github.com>
2023-01-09 14:22:30 +0100

    Delete src/backend/Code directory

--- src/backend/Code/mysql/config.py
+++
@@ -1,6 +0,0 @@
-connection_params = {
-    'host': "projet-yourlist.mysql.database.azure.com",
-    'user': "yourlist_admin@projet-yourlist",
-    'password': "AeHg9SG2eS6TSeC",
-    'database': "projet-yourlist",
-}
